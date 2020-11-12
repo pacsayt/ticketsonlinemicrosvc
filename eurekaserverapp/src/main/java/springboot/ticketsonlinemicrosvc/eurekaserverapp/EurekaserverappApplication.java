1ffe1d9f9163152c23c0834f5706fbc5640ac7d9
Pacsay Tamas <pacsay.tamas@gmail.com>
2020-11-12 14:34:36 +0100

    Modified because of logging + Dockerizing of eurekaserverapp.

--- eurekaserverapp/src/main/java/springboot/ticketsonlinemicrosvc/eurekaserverapp/EurekaserverappApplication.java
+++ eurekaserverapp/src/main/java/springboot/ticketsonlinemicrosvc/eurekaserverapp/EurekaserverappApplication.java
@@ -1,5 +1,7 @@
 package springboot.ticketsonlinemicrosvc.eurekaserverapp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,8 +15,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EurekaserverappApplication
 {
+  private static Logger LOG = LoggerFactory.getLogger( EurekaserverappApplication.class);
+
   public static void main(String[] args)
   {
+    LOG.info( "EurekaserverappApplication::main(String[] args) BEGIN ++++++++++++++++++++++++++++++++++++");
+
     SpringApplication.run(EurekaserverappApplication.class, args);
+
+    LOG.info( "EurekaserverappApplication::main(String[] args) END ++++++++++++++++++++++++++++++++++++");
   }
 }
