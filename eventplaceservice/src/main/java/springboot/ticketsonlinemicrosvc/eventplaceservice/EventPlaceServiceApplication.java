57f1f7567d3449dd3485c363a41597ee330d4824
Pacsay Tamas <pacsay.tamas@gmail.com>
2021-02-10 14:38:51 +0100

    versions in pom.xml, configuration files made all like working

--- eventplaceservice/src/main/java/springboot/ticketsonlinemicrosvc/eventplaceservice/EventPlaceServiceApplication.java
+++ eventplaceservice/src/main/java/springboot/ticketsonlinemicrosvc/eventplaceservice/EventPlaceServiceApplication.java
@@ -26,36 +26,6 @@ import org.springframework.context.annotation.Lazy;
  * pt++ : ++
  * https://www.baeldung.com/spring-cloud-netflix-eureka
  *
- * Spring Boot With H2 Database
- * https://www.baeldung.com/spring-boot-h2-database
- *
- *  pt++ : H2 console : http://localhost:8080/h2-console/ -> JDBC URL : =jdbc:h2:mem:test
- *
- * Session :
- *   Instances may exist in one of the following three states :
- *   transient − A new instance of a persistent class, which is not associated with a Session
- *               and has no representation in the database
- *   persistent − instance has a representation in the database, an identifier value and is
- *                associated with a Session.
- *   detached −  the Hibernate Session is closed
- *
- * Lots of Session's methods is listed :
- * https://www.tutorialspoint.com/hibernate/hibernate_sessions.htm
- *
- * Session session = factory.openSession();
- * Transaction tx = null;
- *
- * try {
- *    tx = session.beginTransaction();
- *    ...
- *    tx.commit();
- * } catch (Exception e) {
- *    if (tx!=null) tx.rollback();
- *    e.printStackTrace();
- * } finally {
- *    session.close();
- * }
- *
  */
 @SpringBootApplication
 @EnableEurekaClient // pt++ : @EnableEurekaServer - must be a separate app, as the server cannot offer services
