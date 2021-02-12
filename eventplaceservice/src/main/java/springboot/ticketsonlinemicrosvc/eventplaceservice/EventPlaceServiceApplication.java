888341261b1eca96b9ca0b480e73c02bfec50c5d
Pacsay Tamas <pacsay.tamas@gmail.com>
2021-02-12 14:40:21 +0100

    @Value() -> @ConfigurationProperties in eventplaceservice, some comments in EventPlaceServiceApplication.java

--- eventplaceservice/src/main/java/springboot/ticketsonlinemicrosvc/eventplaceservice/EventPlaceServiceApplication.java
+++ eventplaceservice/src/main/java/springboot/ticketsonlinemicrosvc/eventplaceservice/EventPlaceServiceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -31,6 +32,7 @@ import org.springframework.context.annotation.Lazy;
 @EnableEurekaClient // pt++ : @EnableEurekaServer - must be a separate app, as the server cannot offer services
 @EnableHystrixDashboard
 @EnableCircuitBreaker
+// @EnableConfigurationProperties(ConfigProperties.class) pt++ : no need for this after S.B. 2.2
 public class EventPlaceServiceApplication implements CommandLineRunner
 {
 	private static final Logger LOG = LoggerFactory.getLogger( EventPlaceServiceApplication.class);
